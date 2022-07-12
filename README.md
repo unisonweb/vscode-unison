485f3da6c567614585b47cca54f1cfb6826a44e4
Chris Penner <christopher.penner@gmail.com>
2022-07-11 23:12:37 -0600

    Initial Setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Unison for Visual Studio Code
+
+## Features
+
+Please see [this page](https://github.com/unisonweb/unison/blob/trunk/docs/language-server.markdown) for a description of supported features.
+
+## Requirements
+
+This plugin requires the [**UCM** (Unison Codebase Manager)](https://github.com/unisonweb/unison).
+
+Ensure an instance of the UCM is running before starting your editor.
+
+## Extension Settings
+
+No settings
+
+## Release Notes
+
+See the [CHANGELOG](./CHANGELOG.md) for updates.
---
+++ src/lsp.ts
@@ -0,0 +1,52 @@
+const { workspace, window } = require("vscode");
+const { LanguageClient } = require("vscode-languageclient/node");
+const { connect } = require("node:net");
+
+const outputChannel = window.createOutputChannel("Unison");
+const clients = new Map();
+
+exports.activate = function () {
+  workspace.workspaceFolders?.forEach((folder) => addWorkspaceFolder(folder));
+  workspace.onDidChangeWorkspaceFolders(({ added, removed }) => {
+    added.forEach((folder) => addWorkspaceFolder(folder));
+    removed.forEach((folder) => removeWorkspaceFolder(folder));
+  });
+};
+
+exports.deactivate = async function () {
+  await Promise.all([...clients.values()].map((client) => client.stop()));
+};
+
+async function addWorkspaceFolder(workspaceFolder) {
+  let folderPath = workspaceFolder.uri.fsPath;
+  if (clients.has(folderPath)) return;
+
+  let client = new LanguageClient("unison", "Unison", connectToServer, {
+    workspaceFolder,
+    outputChannel,
+    documentSelector: [{ language: "unison" }],
+  });
+
+  clients.set(folderPath, client);
+
+  await client.start();
+}
+
+async function removeWorkspaceFolder(workspaceFolder) {
+  let folderPath = workspaceFolder.uri.fsPath;
+  let client = clients.get(folderPath);
+  if (client) {
+    clients.delete(folderPath);
+    await client.stop();
+  }
+}
+
+async function connectToServer() {
+  let socket = connect({ port: 5757, host: "127.0.0.1" });
+
+  await new Promise((resolve, reject) =>
+    socket.once("connect", resolve).once("error", reject)
+  );
+
+  return { reader: socket, writer: socket };
+}
