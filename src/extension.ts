3548a3fe5561c8e697f7240b2cbd8cf02c50a75f
tavish <tavish@paywithextend.com>
2022-11-03 18:03:44 -0500

    Pop open the terminal and run UCM if we dont find it

--- src/extension.ts
+++ src/extension.ts
@@ -52,28 +52,36 @@ async function sleep(ms: number): Promise<void> {
 
 async function connectToServer() {
   let haveShownError = false;
+  let haveOpenedTerminal = false;
+
   while (true) {
     try {
-      const host = "127.0.0.1";
       const port = workspace.getConfiguration("unison").lspPort;
-      log(`Trying to connect to ucm lsp server at ${host}:${port}`);
       let socket = connect({ port, host: "127.0.0.1" });
       await new Promise((resolve, reject) =>
         socket.once("connect", resolve).once("error", reject)
       );
       // Show a success message, but only if we were in an error state
-      const okMsg = `Unison: Connected to Language Server at ${host}:${port}.`;
-      log(okMsg);
       if (haveShownError) {
-        window.showInformationMessage(okMsg);
+        window.showInformationMessage("Unison: Connected to Language Server.");
       }
       return { reader: socket, writer: socket };
     } catch (e) {
-      const errMsg = "Language server failed to connect";
-      log(`${errMsg}, cause: ${e}`)
       if (!haveShownError) {
         haveShownError = true;
-        window.showErrorMessage(`Unison: ${errMsg}, is there a UCM running? (version M4a or later)`);
+        window.showErrorMessage(
+          "Unison: Language server failed to connect, is there a UCM running? (version M4a or later)"
+        );
+
+        // Only ever try to open the terminal once, so we don't get stuck in weird loops
+        // or in a strange state if the user tries to quit UCM or close the terminal.
+        if (!haveOpenedTerminal && workspace.getConfiguration("unison").automaticallyOpenUCM) {
+          // Start up a new terminal in the IDE, tell it to run UCM, and then show it.
+          window.showInformationMessage("Attemping to launch UCM in the embedded terminal.");
+          const terminal = window.createTerminal();
+          terminal.sendText("ucm");
+          terminal.show();
+        }
       }
       await sleep(2000);
       continue;
