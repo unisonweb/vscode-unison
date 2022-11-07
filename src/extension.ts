43d445973d006d811ca8fbbc59f93da0bd0189c2
Tavish Pegram <tapegram@gmail.com>
2022-11-07 15:58:45 -0600

    Pop open the terminal and run UCM if we dont find it (#7)
    * Pop open the terminal and run UCM if we dont find it
    
    * Try to fix weird spacing
    
    * Fixing it for real without autocorrect adding a period?
    
    * Just kidding, 4 spaces not 2
    
    * Remove metadata that snuck in
    
    * Merging in changes from main
    
    * Actually set haveOpenedTerminal
    
    * Turns out haveOpenedTerminal has to live higher up otherwise we still try to reopen the terminal when you close it
    
    * Add ucmCommand config and adjust message pop-ups
    
    Co-authored-by: Chris Penner <christopher.penner@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,12 @@ import { connect } from "node:net";
 
 const outputChannel: OutputChannel = window.createOutputChannel("Unison");
 const clients: Map<string, LanguageClient> = new Map();
+// This is global mutable state so that when the extension host gets restarted after
+// the terminal is closed, we don't immediately open another one.
+let shouldOpenTerminal = true;
 
 function log(msg: string) {
-  outputChannel.appendLine(msg)
+  outputChannel.appendLine(msg);
 }
 
 exports.activate = function () {
@@ -31,7 +34,7 @@ async function addWorkspaceFolder(workspaceFolder: WorkspaceFolder) {
     documentSelector: [{ language: "unison" }],
   });
 
-  log(`Activating unison language server at ${folderPath}`)
+  log(`Activating unison language server at ${folderPath}`);
   clients.set(folderPath, client);
   await client.start();
 }
@@ -40,7 +43,7 @@ async function removeWorkspaceFolder(workspaceFolder: WorkspaceFolder) {
   let folderPath = workspaceFolder.uri.fsPath;
   let client = clients.get(folderPath);
   if (client) {
-    log(`Deactivating unison language server at ${folderPath}`)
+    log(`Deactivating unison language server at ${folderPath}`);
     clients.delete(folderPath);
     await client.stop();
   }
@@ -52,15 +55,19 @@ async function sleep(ms: number): Promise<void> {
 
 async function connectToServer() {
   let haveShownError = false;
+
   while (true) {
     try {
       const host = "127.0.0.1";
       const port = workspace.getConfiguration("unison").lspPort;
+
       log(`Trying to connect to ucm lsp server at ${host}:${port}`);
       let socket = connect({ port, host: "127.0.0.1" });
       await new Promise((resolve, reject) =>
         socket.once("connect", resolve).once("error", reject)
       );
+
+      shouldOpenTerminal = false;
       // Show a success message, but only if we were in an error state
       const okMsg = `Unison: Connected to Language Server at ${host}:${port}.`;
       log(okMsg);
@@ -70,10 +77,26 @@ async function connectToServer() {
       return { reader: socket, writer: socket };
     } catch (e) {
       const errMsg = "Language server failed to connect";
-      log(`${errMsg}, cause: ${e}`)
-      if (!haveShownError) {
+      log(`${errMsg}, cause: ${e}`);
+
+      // Only ever try to open the terminal once, so we don't get stuck in weird loops
+      // or in a strange state if the user tries to quit UCM or close the terminal.
+      if (
+        shouldOpenTerminal &&
+        workspace.getConfiguration("unison").automaticallyOpenUCM
+      ) {
+        let ucmCommand = workspace.getConfiguration("unison").ucmCommand;
+        shouldOpenTerminal = false;
+        log("Opening ucm terminal");
+        // Start up a new terminal in the IDE, tell it to run UCM, and then show it.
+        const terminal = window.createTerminal();
+        terminal.sendText(ucmCommand);
+        terminal.show();
+      } else if (!haveShownError) {
         haveShownError = true;
-        window.showErrorMessage(`Unison: ${errMsg}, is there a UCM running? (version M4a or later)`);
+        window.showErrorMessage(
+          `Unison: ${errMsg}, is there a UCM running? (version M4a or later)`
+        );
       }
       await sleep(2000);
       continue;
